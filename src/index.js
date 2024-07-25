b826444f86e574729a3b2b89fc014b4279c33b07
RudrakshTaya <124330693+RudrakshTaya@users.noreply.github.com>
2024-07-25 09:36:51 +0000

    database connected-2

--- src/index.js
+++ src/index.js
@@ -5,7 +5,16 @@ dotenv.config({
 })
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log(`server is running at http://localhost:${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("error",err);
+})
