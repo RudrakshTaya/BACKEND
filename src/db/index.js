5172d9ecf84becd459613077cceac4d807a0470e
RudrakshTaya <124330693+RudrakshTaya@users.noreply.github.com>
2024-08-29 06:42:34 +0000

    routes and controllers-1

--- src/db/index.js
+++ src/db/index.js
@@ -1,11 +1,11 @@
 
 
 import mongoose from "mongoose";
-import { DB_NAME } from "../constants.js";
+//import { DB_NAME } from "../constants.js";
 
 const connectDB = async () => {
   try {
-    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}`);
     console.log(`\nMongoDB connected: ${connectionInstance.connection.host}`);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
